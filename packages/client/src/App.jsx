253b384fefb4b3d26db834fdcd427a7c64e64de8
Nguyễn Đăng Khoa <145103457+NguyenDangKhoa252789@users.noreply.github.com>
2023-10-18 16:23:04 +0700

    Add: router

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,13 +1,28 @@
 /* eslint-disable no-undef */
 import React from "react";
 import "./App.css";
-import BasicModal from "@pages/todo-management";
+import { Link, Route, Routes } from "react-router-dom";
 import { TodoContextProvider } from "@context/TodoContextProvider";
+import TodoPage from "./pages/todo-management/index";
+import Home from "./pages/Home-management/index";
 
 export default function App() {
   return (
     <TodoContextProvider>
-      <BasicModal />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/todopage">Todo</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todopage" element={<TodoPage />} />
+      </Routes>
     </TodoContextProvider>
   );
 }
--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -1,3 +1,11 @@
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
